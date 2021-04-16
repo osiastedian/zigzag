e4da9a5a3a21e2060ee9f4122045769db6f7b9ac
Ted Ian Osias <ted.osias@trustarc.com>
2021-04-16 20:01:01 +0800

    initial commit

---
+++ apps/robot-factory/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'robot-factory',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/robot-factory',
+};
