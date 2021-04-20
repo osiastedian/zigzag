69e5d0365cde52375e50fe4522e3e606bd098521
Ted Ian Osias <ted.osias@trustarc.com>
2021-04-20 20:50:37 +0800

    refactor(): update nx implicit deps and fix formatting

--- apps/robot-factory-api/src/app/app.module.ts
+++ apps/robot-factory-api/src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { Module, Scope } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RobotsController } from './robots/robots.controller';
 import { RobotsService } from './robots/robots.service';
-import { ShipmentsController } from './shipments/shipments.controller';
+
 import { ShipmentService } from './shipment/shipment.service';
 import { ShipmentController } from './shipment/shipment.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, RobotsController, ShipmentsController, ShipmentController],
+  controllers: [
+    AppController,
+    RobotsController,
+    ShipmentController,
+    ShipmentController,
+  ],
   providers: [
     AppService,
     {
